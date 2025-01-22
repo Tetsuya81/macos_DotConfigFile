8b83cc90fec6ebae9398c11fd4185244c878c8ab
Tetsuya81 <tetsuya8109@gmail.com>
2025-01-22 13:16:24 +0900

    Change WezTerm's theme
    Change WezTerm to switch themes to match the appearance of macOS.
    
    Change to use theme.
    'tokyonigiht' >> 'Tomorrow (dark) (terminal.sexy)' & 'Tomorrow (light) (terminal.sexy)'

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,13 +1,22 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
--- This will hold the configuration.
-local config = wezterm.config_builder()
+-- wezterm.gui is not available to the mux server, so take care to
+-- do something reasonable when this config is evaluated by the mux
+function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
 
--- This is where you actually apply your config choices
+function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Tomorrow (dark) (terminal.sexy)'
+  else
+    return 'Tomorrow (light) (terminal.sexy)'
+  end
+end
 
--- For example, changing the color scheme:
-config.color_scheme = 'tokyonight'
-
--- and finally, return the configuration to wezterm
-return config
+return {
+  color_scheme = scheme_for_appearance(get_appearance()),
+}
