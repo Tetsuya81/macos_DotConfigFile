3e164d72bcff188a047768f75653e979971db119
Tetsuya81 <tetsuya8109@gmail.com>
2025-03-20 11:15:59 +0900

    Update configuration files and improve .gitignore entries

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,22 +1,10 @@
 local wezterm = require 'wezterm'
 
--- wezterm.gui is not available to the mux server, so take care to
--- do something reasonable when this config is evaluated by the mux
-function get_appearance()
-  if wezterm.gui then
-    return wezterm.gui.get_appearance()
-  end
-  return 'Dark'
-end
+local theme = require 'theme'
+local shortcuts = require 'shortcuts'
 
-function scheme_for_appearance(appearance)
-  if appearance:find 'Dark' then
-    return 'Tomorrow (dark) (terminal.sexy)'
-  else
-    return 'Tomorrow (light) (terminal.sexy)'
-  end
-end
 
-return {
-  color_scheme = scheme_for_appearance(get_appearance()),
-}
+local config = {}
+  theme.apply_to_config(config)
+  shortcuts.apply_to_config(config)
+return config
